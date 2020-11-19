f16b2c4b0e128077a538eca3a00152bde16fc8f3
Damian Babiński <d.babinski94@gmail.com>
2020-11-19 13:11:26 +0100

    Initial_commit

---
+++ app/src/main/java/pl/mobile/divinumofficium/MainActivity.java
@@ -0,0 +1,56 @@
+package pl.mobile.divinumofficium;
+
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(" ");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
---
+++ app/src/main/java/pl/mobile/divinumofficium/ui/ScrollFab.java
@@ -0,0 +1,60 @@
+package pl.mobile.divinumofficium.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import pl.mobile.divinumofficium.R;
+
+public class ScrollFab extends FloatingActionButton.Behavior {
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+                                       FloatingActionButton child,
+                                       View directTargetChild,
+                                       View target,
+                                       int axes, int type) {
+
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout,
+                               FloatingActionButton child,
+                               View target, int dxConsumed,
+                               int dyConsumed, int dxUnconsumed,
+                               int dyUnconsumed, int type,
+                               int[] consumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, consumed);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE){
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onHidden(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE){
+
+            child.show();
+        }
+
+    }
+
+    public ScrollFab (Context context, AttributeSet attrs) {
+        super();
+    }
+
+
+
+}
+
